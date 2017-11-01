cc26cfe7c58d951f8aeb4b2d0fc94eca861830da
Vincent Richerson-146004265 <146004265@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:53:12 -0700

    pieces

--- src/blokus/Piece.java
+++ src/blokus/Piece.java
@@ -1,7 +1,54 @@
 
 package blokus;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 
-public class Piece {
-    //long bong dong
+
+public class Piece{
+      public static enum Block {
+      ONE,TWO,THREE,F,T,V,Z,W,U,L,I,X,P,Y,N,SHORTT,SHORTZ,SQAURE,CROOKED3,SHORTI,SHORTL
+    }; 
+      
+       private Color color;
+        private int random;
+        private Block block;
+          Piece(Color _color,Block _block)
+    {
+        color = _color;
+        block = _block;
+        random = (int)(  Math.random()*4 + 1 );
+
+    }
+            Color getColor()
+    {
+        return (color);
+    }
+    int getRandom()
+    {
+        return (random);
+    }
+
+    public void draw(Graphics2D g,int row,int column,
+    int xdelta,int ydelta) {
+        
+        g.setColor(color); 
+        g.fillRect(Window.getX(column*xdelta),
+        Window.getY(row*ydelta),xdelta,ydelta);
+         g.setColor(Color.white);
+            g.setFont(new Font("Arial",Font.BOLD,25));
+            g.drawString("" + random, Window.getX(column*xdelta)+30,
+        Window.getY(row*ydelta)+35);
+    }
+    public void one(Graphics2D g,int row,int column,
+    int xdelta,int ydelta){
+         g.setColor(color); 
+        g.fillRect(Window.getX(column*xdelta),
+        Window.getY(row*ydelta),xdelta,ydelta);
+         g.setColor(Color.white);
+            g.setFont(new Font("Arial",Font.BOLD,25));
+            g.drawString("" + random, Window.getX(column*xdelta)+30,
+        Window.getY(row*ydelta)+35);
+    }
 }
