00bb6a25bbaa890721c2a0faadcd8fe88416c34c
Dylan Marvin-150003044 <150003044@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:54:54 -0700

    Player

--- src/blokus/Player.java
+++ src/blokus/Player.java
@@ -6,22 +6,34 @@ import java.awt.Color;
 public class Player {
     private static Player currentTurn;
     private static int numPlayers;
-    private static Player players[] = new Player[numPlayers];
+    private static Player players[] = new Player[4];
     private int points;
     private Color color;
     private int playerNum;
     
     public static void Reset(){
         if(players[0] == null){
-            players[0] = new Player(Color.red,1);
-            players[1] = new Player(Color.blue,2);
+            for(int i = 0;i<numPlayers;i++){
+                players[i] = new Player(i);
+            }
         }   
         currentTurn = players[0];
     }
     
-    Player(Color _color,int _num){
-        color = _color;
+    Player(int _num){       
         playerNum = _num;
+        if(playerNum == 1){
+            color = Color.blue;
+        }
+        else if(playerNum == 2){
+            color = Color.yellow;
+        }
+        else if(playerNum == 3){
+            color = Color.red;
+        }
+        else if(playerNum == 4){
+            color = Color.green;
+        }
         
     }
     public Color getColor(){
@@ -44,18 +56,7 @@ public class Player {
         else 
             return players[1];
         
-    }
-    
-    public static void TurnChange(){
-        
-       if(currentTurn == players[0]){
-            currentTurn = players[1];
-       }
-        else if(currentTurn == players[1]){
-            currentTurn = players[0];
-        }
-       
-    }
+    }   
     public static void AddPoints(int player, int _points){
         players[player].points += _points;
     }
@@ -63,17 +64,11 @@ public class Player {
     public static int GetPoints(int player){
         return players[player].points;
     }
-    public static Player GetOtherPlayer(){
-        if(currentTurn == players[0])
-            return players[1];
-        return players[0];
+    public static Player GetNextPlayer(){             
+        return players[(currentTurn.getPlayerNum()+1)];
     }
-    public static Player getPlayer1(){
-        return players[0];
+    public static Player getPlayer(int playerNumber){
+        return players[playerNumber];
     }
-    public static Player getPlayer2(){
-        return players[1];
-    }
-
 }
 
