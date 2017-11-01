54c34274cb673cdd17b9000b64400175e0fd4915
Vincent Richerson-146004265 <146004265@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:54:05 -0700

    piecesupdate

--- src/blokus/Piece.java
+++ src/blokus/Piece.java
@@ -14,6 +14,8 @@ public class Piece{
        private Color color;
         private int random;
         private Block block;
+        private int row;
+        private int column;
           Piece(Color _color,Block _block)
     {
         color = _color;
@@ -30,9 +32,10 @@ public class Piece{
         return (random);
     }
 
-    public void draw(Graphics2D g,int row,int column,
+    public void draw(Graphics2D g,int _row,int _column,
     int xdelta,int ydelta) {
-        
+        row = _row;
+        column = _column;
         g.setColor(color); 
         g.fillRect(Window.getX(column*xdelta),
         Window.getY(row*ydelta),xdelta,ydelta);
@@ -40,15 +43,25 @@ public class Piece{
             g.setFont(new Font("Arial",Font.BOLD,25));
             g.drawString("" + random, Window.getX(column*xdelta)+30,
         Window.getY(row*ydelta)+35);
+            
     }
-    public void one(Graphics2D g,int row,int column,
-    int xdelta,int ydelta){
-         g.setColor(color); 
-        g.fillRect(Window.getX(column*xdelta),
-        Window.getY(row*ydelta),xdelta,ydelta);
-         g.setColor(Color.white);
-            g.setFont(new Font("Arial",Font.BOLD,25));
-            g.drawString("" + random, Window.getX(column*xdelta)+30,
-        Window.getY(row*ydelta)+35);
-    }
+   public void one(Piece board[][]){
+       board[row][column] = this;
+   }
+    public void two(Piece board[][]){
+       board[row][column] = this;
+        board[row+1][column] = this;
+   }
+      public void three(Piece board[][]){
+       board[row][column] = this;
+        board[row+1][column] = this;
+          board[row+2][column] = this;
+   }
+        public void F(Piece board[][]){
+       board[row][column] = this;
+        board[row-1][column] = this;
+          board[row][column+1] = this;
+            board[row][column-1] = this;
+             board[row+1][column+1] = this;
+   }
 }
