dd66fb6f3c72cdcb469f550cec1f5f94e0254061
Long Truong-156004533 <156004533@vusdnoc.vusd.k12.ca.us>
2017-11-03 10:59:15 -0700

    Vincent sucks

---
+++ src/blokus/Outside_Pieces.java
@@ -0,0 +1,79 @@
+
+package blokus;
+
+import java.io.*; 
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Outside_Pieces extends Piece{
+    
+    Outside_Pieces(Color color, Block block){
+        super(color, block);
+    }
+    
+    public void drawPiece1(){
+        
+    }
+    public void drawPiece2(){
+        
+    }
+    public void drawPiecei3(){
+        
+    }
+    public void drawPiecei4(){
+        
+    }
+    public void drawPiecei5(){
+        
+    }
+    public void drawPieceV3(){
+        
+    }
+    public void drawPieceL4(){
+        
+    }
+    public void drawPieceL5(){
+        
+    }
+    public void drawPieceT4(){
+        
+    }
+    public void drawPieceT5(){
+        
+    }
+    public void drawPieceO(){
+        
+    }
+    public void drawPieceW(){
+        
+    }
+    public void drawPieceU(){
+        
+    }
+    public void drawPieceX(){
+        
+    }
+    public void drawPieceP(){
+        
+    }
+    public void drawPieceY(){
+        
+    }
+    public void drawPieceF(){
+        
+    }
+    public void drawPieceN(){
+        
+    }
+    public void drawPieceZ4(){
+        
+    }
+    public void drawPieceZ5(){
+        
+    }
+    public void drawPieceV5(){
+        
+    }    
+}
