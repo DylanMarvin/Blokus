01d5cabfdf02a497eec52a2439881bc29e4c2704
Vincent Richerson-146004265 <146004265@vusdnoc.vusd.k12.ca.us>
2017-11-17 10:26:12 -0800

    please work

--- src/blokus/Board.java
+++ src/blokus/Board.java
@@ -89,252 +89,218 @@ public class Board {
             }
             
             if (piece.getBlock() == Piece.Block.ONE) { 
-                if(checkPieceH(zrow,zcol,1,rotation) && checkPieceV(zrow,zcol,1,rotation)){
+               
                     board[zrow][zcol] = piece;
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(0);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             
             else if (piece.getBlock() == Piece.Block.TWO) {
-                if(checkPieceH(zrow,zcol,2,rotation) && checkPieceV(zrow,zcol,2,rotation)){
+               
 
                     piece.Two(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(1);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.I3) { 
-                if(checkPieceH(zrow,zcol,3,rotation ) && checkPieceV(zrow,zcol,3,rotation)){
+                
                     piece.I3(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(2);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.I4) {         
-                if(checkPieceH(zrow,zcol,4,rotation) && checkPieceV(zrow,zcol,4,rotation)){
+               
                     piece.I4(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(3);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.I5) {  
-                if(checkPieceH(zrow,zcol,5,rotation) && checkPieceV(zrow,zcol,5,rotation)){
+              
                     piece.I5(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(4);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.L4) {
-                if(checkPieceL(zrow,zcol,3,rotation)){
+                
                     piece.L4(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(6);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
                 }
-            }
+            
             else if (piece.getBlock() == Piece.Block.L5) { 
-                if(checkPieceL(zrow,zcol,4,rotation)){
+               
                     piece.L5(board,rotation);                    
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(7); 
                     Player.GetCurrentPlayer().setCompletedC(Challenge.checkChallenge());
                     if(Player.getCompletedC() == false)
                         Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.N) {        
-                if(checkPieceN(zrow,zcol,4,rotation)){
+              
                     piece.N(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(17);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.O) { 
-                if(checkPieceO(zrow,zcol,2,rotation)){
+               
                     piece.O(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(10);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }           
+                          
             }
             else if (piece.getBlock() == Piece.Block.W) {  
-                if(checkPieceW(zrow,zcol,2,rotation)){
+                
                     piece.W(board,rotation);
                     Player.GetCurrentPlayer().getOutsidePieces().changePiece(11);
                     Player.switchTurns();
                     Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }           
+                           
             
             }
             else if (piece.getBlock() == Piece.Block.T4) {  
-                 if(checkPieceT4(zrow,zcol,4,rotation)){
+                 
                 piece.T4(board,rotation);
                 Player.GetCurrentPlayer().getOutsidePieces().changePiece(9);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                 }
+                 
             }
             else if (piece.getBlock() == Piece.Block.T5) {    
-                if(checkPieceT5(zrow,zcol,5,rotation)){
+                
                 piece.T5(board,rotation);              
                 Player.GetCurrentPlayer().getOutsidePieces().changePiece(8);          
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.U) {
 
-                if(checkPieceU(zrow,zcol,5,rotation)){
+                
                     piece.U(board,rotation);
                 Player.GetCurrentPlayer().getOutsidePieces().changePiece(12);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
             else if (piece.getBlock() == Piece.Block.V3) {
-                if(checkPieceV3(zrow,zcol,5,rotation)){                
+                            
                 piece.V3(board,rotation);
                 Player.GetCurrentPlayer().getOutsidePieces().changePiece(5);
                 Player.GetCurrentPlayer().setCompletedC(Challenge.checkChallenge());
                 if(Player.getCompletedC() == false)
                     Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
 
             else if (piece.getBlock() == Piece.Block.V5) {
-              if(checkPieceV5(zrow,zcol,5,rotation)){                
+                              
                 piece.V5(board,rotation);
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(20);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-              }
+              
             }
 
             else if (piece.getBlock() == Piece.Block.F) {
-                if(checkPieceF(zrow,zcol,5,rotation)){                
+                               
                 piece.F(board,rotation);
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(16);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
 
             else if (piece.getBlock() == Piece.Block.P) { 
-                if(checkPieceP(zrow,zcol,5,rotation)){
+               
                 piece.P(board,rotation);              
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(14);
                 Player.switchTurns();
-                }
+                
             }
 
             else if (piece.getBlock() == Piece.Block.X) {
-                if(checkPieceX(zrow,zcol,5,rotation)){
+               
                 piece.X(board,rotation);
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(13);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-                }
+                
             }
 
             else if (piece.getBlock() == Piece.Block.Y) {
-                if(checkPieceY(zrow,zcol,5,rotation)){                
+                       
                 piece.Y(board,rotation);
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(15);
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-            }
+            
             }
             else if (piece.getBlock() == Piece.Block.Z4) {
-                 if(checkPieceZ4(zrow,zcol,5,rotation)){    
+                
                 piece.Z4(board,rotation);
                    Player.GetCurrentPlayer().getOutsidePieces().changePiece(18);
                 if(Player.getCompletedC() == false)
                     Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-            }
+            
             }
             else if (piece.getBlock() == Piece.Block.Z5) {                
 
-                if(checkPieceZ5(zrow,zcol,5,rotation)){    
+                 
                 piece.Z5(board,rotation);
                 Player.GetCurrentPlayer().getOutsidePieces().changePiece(19);
                 Player.GetCurrentPlayer().setCompletedC(Challenge.checkChallenge());
                 if(Player.getCompletedC() == false)
                 Player.switchTurns();
                 Challenge.setChallenges(Player.GetCurrentPlayer().getOutsidePieces());
-            }
+            
             }
             
 
         }
     }
    //////////////////////////////////////////////////////////////////////////////
-    private static boolean checkPieceH(int zrow,int zcol,int val,int rotation){
-        for(int i = 0;i<val;i++){
-            if(zcol+i == NUM_COLUMNS){
-                return false;
-            }
-            if(board[zrow][zcol+i] != null){
-                       return false;
-            }    
-        }
-        return(true);
-    }
-    private static boolean checkPieceV(int zrow,int zcol,int val,int rotation){
-        for(int i = 0;i<val;i++){
-            if(zrow+i == NUM_ROWS){
-                return false;
-            }
-            if(board[zrow+i][zcol] != null){
-                       return false;
-            }    
-        }
-        return(true);
-    }
-    private static boolean checkPieceL(int zrow,int zcol,int val,int rotation){
-        
-            for(int j = 0;j<2;j++){
-                    if(zcol+j == NUM_COLUMNS){
-                        return false;
-                    }               
-                for(int i = 0;i<val;i++){
-                    if(zrow+i == NUM_ROWS){
-                        return false;
-                    }
-                    if(j >0){
-                        i = val-1;
-                    }
-                    if(board[zrow+i][zcol+j] != null){
-                               return false;
-                    }    
-                }
-            }
-            return true;
-        }
+   
+   
     private static boolean checkPieceO(int zrow,int zcol,int val,int rotation){
-        for(int j = 0;j<val;j++){
-            if(zcol+j == NUM_COLUMNS){
-                    return false;
-                }
-            for(int i = 0;i<val;i++){
-                if(zrow+i == NUM_ROWS){
-                    return false;
-                }
-                if(board[zrow+i][zcol+j] != null){
-                           return false;
-                }    
-            }
+          
+             if(board[zrow][zcol] == null &&
+        board[zrow][zcol + 1] == null &&
+        board[zrow + 1][zcol] == null &&
+        board[zrow + 1][zcol + 1] == null){
+            return(true);
         }
-        return(true);
-    }
+       
+             
+        return(false);
+        }
+    private static boolean checkPieceOne(int zrow,int zcol,int val,int rotation){
+          
+             if(
+         board[zrow][zcol] == null){
+            return(true);
+        }
+       
+             
+        return(false);
+        }
    ////////////////////////////////////////////////////////////////////////////////////////////// 
     private static boolean checkPieceN(int zrow,int zcol,int val,int rotation){
         if(rotation == 00){
